487b170516ff5bb43867093283fb73d85699229b
SirGlaucus <glaucusjoseph@gmail.com>
2024-01-08 11:31:37 -0500

    Base for the form and styles

--- src/Views/HomeScreen/index.tsx
+++ src/Views/HomeScreen/index.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./index.css";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 main-box">
+      <div className="container file-upload-box">
+        <button className="leave-button">X</button>
+        <div className="file-upload-box-form">
+          <h1 className="title underline">Document Upload</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;

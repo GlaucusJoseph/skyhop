f1041ae0133bc3abab057e7d1b825be33be695a6
SirGlaucus <glaucusjoseph@gmail.com>
2024-01-08 13:03:06 -0500

    Adding svg icons

---
+++ src/assets/svg/PictureIcon.tsx
@@ -0,0 +1,60 @@
+import { IconProps } from "../../Interfaces";
+
+export const PictureIcon: React.FC<IconProps> = ({ width, height }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width={width}
+      height={height}
+      viewBox="0,0,256,256"
+    >
+      <g transform="">
+        <g
+          fill="none"
+          fill-rule="nonzero"
+          stroke="none"
+          stroke-width="1"
+          stroke-linecap="butt"
+          stroke-linejoin="miter"
+          stroke-miterlimit="10"
+          stroke-dasharray=""
+          stroke-dashoffset="0"
+          font-family="none"
+          font-weight="none"
+          font-size="none"
+          text-anchor="none"
+          style={{ mixBlendMode: "normal" }}
+        >
+          <g transform="translate(-0.50255,0.00877) scale(3.2,3.2)">
+            <path
+              d="M12.5,75.5v-71h37.293l17.707,17.707v53.293z"
+              fill="#ffffff"
+            ></path>
+            <path
+              d="M49.586,5l17.414,17.414v52.586h-54v-70h36.586M50,4h-38v72h56v-54l-18,-18z"
+              fill="#fa9c27"
+            ></path>
+            <path
+              d="M49.5,22.5v-18h0.293l17.707,17.707v0.293z"
+              fill="#ffffff"
+            ></path>
+            <path
+              d="M50,5.414l16.586,16.586h-16.586v-16.586M50,4h-1v19h19v-1l-18,-18z"
+              fill="#fa9c27"
+            ></path>
+            <path
+              d="M46,42.338l-6,5.662l9.108,10h6.892v-6z"
+              fill="#fa9c27"
+            ></path>
+            <path
+              d="M52,30c-2.20914,0 -4,1.79086 -4,4c0,2.20914 1.79086,4 4,4c2.20914,0 4,-1.79086 4,-4c0,-2.20914 -1.79086,-4 -4,-4zM52,58h-28v-8l10,-10z"
+              fill="#fa9c27"
+            ></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,4 @@
+import { FileIcon } from "./FileIcon";
+import { PictureIcon } from "./PictureIcon";
+
+export { FileIcon, PictureIcon };

cbe81525dc4cdb22edaf50e9befde5e7bcfc4c67
SirGlaucus <glaucusjoseph@gmail.com>
2024-01-09 15:47:27 -0500

    The whole box can be clicked to ge the file

--- src/Components/UploadDragAndDrop/index.tsx
+++ src/Components/UploadDragAndDrop/index.tsx
@@ -1,5 +1,6 @@
 import "./index.css";
 import { FileIcon } from "../../assets/svg";
+import { useRef } from "react";
 
 interface UploadDragAndDropProps {
   setSelectedFile: (file: File | null) => void;
@@ -8,6 +9,8 @@ interface UploadDragAndDropProps {
 const UploadDragAndDrop: React.FC<UploadDragAndDropProps> = ({
   setSelectedFile,
 }) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
@@ -27,18 +30,25 @@ const UploadDragAndDrop: React.FC<UploadDragAndDropProps> = ({
       setSelectedFile(file);
     }
   };
+
+  const handleDivClick = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <div className="dnd-box-external">
       <div
         className="dnd-box-internal"
         onDragOver={handleDragOver}
         onDrop={handleDrop}
+        onClick={handleDivClick}
       >
         <input
           type="file"
           onChange={handleChange}
           className="hide-input"
           id="fileInput"
+          ref={fileInputRef}
         />
         <div className="container mb-2">
           <FileIcon width={"30"} height={"30"} />

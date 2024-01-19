ddd6b15e20a73b8f1bb82f7c4169df83cd4db0c9
SirGlaucus <glaucusjoseph@gmail.com>
2024-01-18 19:11:34 -0500

    Adding React.memo

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { UseFormRegister, FieldValues } from "react-hook-form";
 import { ClockIcon } from "../../assets/svg/ClockIcon";
 import { FormValues } from "../../Interfaces";
@@ -9,36 +10,33 @@ interface SelectProps<TFormValues extends FieldValues> {
   options: Array<{ value: string; label: string }>;
 }
 
-const Select: React.FC<SelectProps<FormValues>> = ({
-  register,
-  name,
-  label,
-  options,
-}) => {
-  return (
-    <div className="d-flex justify-content-between align-items-center mt-4">
-      <div>
-        <span className="special-text">{label}</span>
-      </div>
-      <div className="d-flex align-items-center">
-        <select
-          id={name}
-          className="form-select special-text me-2"
-          {...register(name)}
-        >
-          <option value="" disabled>
-            Select Client
-          </option>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
+const Select: React.FC<SelectProps<FormValues>> = React.memo(
+  ({ register, name, label, options }) => {
+    return (
+      <div className="d-flex justify-content-between align-items-center mt-4">
+        <div>
+          <span className="special-text">{label}</span>
+        </div>
+        <div className="d-flex align-items-center">
+          <select
+            id={name}
+            className="form-select special-text me-2"
+            {...register(name)}
+          >
+            <option value="" disabled>
+              Select Client
             </option>
-          ))}
-        </select>
-        <ClockIcon width={"30"} height={"30"} />
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <ClockIcon width={"30"} height={"30"} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Select;
